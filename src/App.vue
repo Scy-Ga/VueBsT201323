71e7b0d4796260a8fefedbdd6e8dd233640765d5
VargaGabor <gabor.varga87@hotmail.com>
2020-10-23 14:21:33 +0200

    Up

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
+
     <jobSelect />
+    
   </div>
 </template>
 
--- src/components/jobSelect.vue
+++ src/components/jobSelect.vue
@@ -35,18 +35,7 @@
       </b-modal>
     </div>
   
-     <div>
-    <b-button id="show-btn" @click="showModal">Open Modal</b-button>
-    <b-button id="toggle-btn" @click="toggleModal">Toggle Modal</b-button>
-
-    <b-modal ref="my-modal" hide-footer title="Using Component Methods">
-      <div class="d-block text-center">
-        <h3>Hello From My Modal!</h3>
-      </div>
-      <b-button class="mt-3" variant="outline-danger" block @click="hideModal">Close Me</b-button>
-      <b-button class="mt-2" variant="outline-warning" block @click="toggleModal">Toggle Me</b-button>
-    </b-modal>
-  </div>
+     
   </div>
 </template>
 
