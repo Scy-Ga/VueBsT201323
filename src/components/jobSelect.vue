a64d09db7851d61fc74517602a1bf306bfc94e6b
VargaGabor <gabor.varga87@hotmail.com>
2020-10-23 13:29:12 +0200

    Up

---
+++ src/components/jobSelect.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class jobSelect extends Vue {
+  
+}
+</script>
+
+<style>
+
+</style>
